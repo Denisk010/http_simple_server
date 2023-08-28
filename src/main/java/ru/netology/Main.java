366d0835ca6bb9c0ea8b656eeb9866b8992bd911
Denisk010 <122920660+Denisk010@users.noreply.github.com>
2023-08-28 23:12:08 +0300

    Update Main.java

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,7 +1,30 @@
 package ru.netology;
+
+import java.io.IOException;
+
 public class Main {
+    final static int PORT = 9999;
+
     public static void main(String[] args) {
+
         Server server = new Server();
-        server.startHTTPServer(9999);
+
+        for (String validPath : ClientHandler.validPaths) {
+            server.addHandler("GET", validPath, (request, responseStream) -> {
+                try {
+                    ClientHandler.responseOK(request, responseStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        server.addHandler("POST", "/resources.html", (request, responseStream) -> {
+            try {
+                ClientHandler.responseOK(request, responseStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        server.startHTTPServer(PORT);
     }
 }
